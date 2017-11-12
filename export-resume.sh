08fc73d015b09ad80ed6aade92d0eb154d05498b
grimsleydl <gromzly@DESKTOP-USIKH5A.localdomain>
2017-11-12 15:04:24 -0600

    forgot I didn't hardcode in support for node options in the html-pdf cli.

--- export-resume.sh
+++ export-resume.sh
@@ -9,7 +9,7 @@ sed -i -e 's/ till / – /g' resumes/$(basename "${1%.*}").html
 sed -i -e 's/list-style: square;/list-style: disc;/g' resumes/$(basename "${1%.*}").html
 sed -i -e 's/<h2>Highlights/<h2>Skills/g' resumes/$(basename "${1%.*}").html
 
-html-pdf "resumes/$(basename "${1%.*}").html" "resumes/$(basename "${1%.*}").pdf" \
-    --format=Letter
+html-pdf "resumes/$(basename "${1%.*}").html" "resumes/$(basename "${1%.*}").pdf"
+    # --format=Letter
 #     --border=.5in \
 #     --border.left=1in --border.right=1in --border.top=1in --border.bottom=1in --header.height=0 footer.height=0
